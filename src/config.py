cea5fc9c0d3d850de421ca1a2ed33c0021564b29
Zaher Arman <a.zaher@mail.utoronto.ca>
2026-03-26 18:49:37 -0400

    Additions + caps

--- src/config.py
+++ src/config.py
@@ -4,25 +4,36 @@ import os
 from pathlib import Path
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import Field
+from typing import Optional
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file= BASE_DIR / ".env", env_file_encoding="utf-8", case_sensitive=False, extra="ignore")
+    
+    DATA_DIR: Path = BASE_DIR / "data"
 
-    llm_base_url: str = "http://localhost:11434"
-    llm_model_name:  str = "mistral:latest"
-    llm_temp: float = Field(0.7, ge=0.0, le=2.0)
-    llm_max_tokens: int = Field (2048, gt=0)
-    llm_timeout: int = Field(120, gt=0)
+    LLM_PROVIDER: str = "openai_compatible"
+    LLM_BASE_URL: str
+    LLM_MODEL_NAME:  str
+    LLM_TEMP: float = Field(0.7, ge=0.0, le=2.0)
+    LLM_MAX_TOKENS: int = Field (2048, gt=0)
+    LLM_TIMEOUT: int = Field(120, gt=0)
 
-    data_dir: Path = BASE_DIR / "data"
+    EMBEDDING_PROVIDER: str = "sentence_transformers" 
+    EMBEDDING_MODEL_NAME: str = "all-MiniLM-L6-v2"
+    EMBEDDING_BASE_URL: Optional[str] = None
+    EMBEDDING_API_KEY: Optional[str] = None
+    
+    TOP_K: int = Field(5, gt=0)
+    CHUNK_SIZE: int = Field(1000, gt=0) # Max size of chunks in charactors
+    CHUNK_OVERLAP: int = Field(300, ge=0)
 
-    embedding_model_name: str = "all-MiniLM-L6-v2"
-    top_k: int = Field(5, gt=0)
-    chunk_size: int = Field(1000, gt=0) # Max size of chunks in charactors
-    chunk_overlap: int = Field(300, ge=0)
+    OCR_PROVIDER: Optional[str] = None
+    AZURE_MISTRAL_OCR_MODEL: str
 
+    AZURE_API_KEY: str
+    
     NEO4J_URI: str
     NEO4J_USERNAME: str
     NEO4J_PASSWORD: str
